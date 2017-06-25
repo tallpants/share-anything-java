7f3f216f0551185f11d4e16baf0a280da26c325b
Adithya Reddy <adithyakreddy6@gmail.com>
2017-06-25 21:06:38 +0530

    Share skeleton

---
+++ src/main/java/com/tallpants/shareanything/Share.java
@@ -0,0 +1,35 @@
+package com.tallpants.shareanything;
+
+import com.mashape.unirest.http.exceptions.UnirestException;
+
+import java.io.File;
+import java.net.URL;
+
+public class Share {
+  public static URL share(File file) throws UnirestException {
+    String split[] = file.getName().split("\\.");
+    if (split.length < 2) {
+      // anonfile
+      return null;
+    }
+
+    String fileExtension = split[split.length - 1];
+
+    if (fileExtension.equals("shareanythingsnippet")) {
+      // gist raw
+      return null;
+    }
+
+    double sizeMB = Math.ceil(file.length() / 1000000);
+
+    if (sizeMB <= 10 && Extensions.imgur.contains(fileExtension)) {
+      return Hosts.imgur(file);
+    } else if (sizeMB <= 1 && Extensions.gist.contains(fileExtension)) {
+      // gist
+      return null;
+    } else {
+      // anonfile
+      return null;
+    }
+  }
+}
